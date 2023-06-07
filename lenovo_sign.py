8baa1234c67fe4598b7b7c07744ea9d4d06edd1b
AlanZhao <a@mxlt.top>
2023-06-07 17:48:14 +0800

    Update lenovo_sign.py

--- lenovo_sign.py
+++ lenovo_sign.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-13 59 1 * * *
+"""
 File: lenovo_sing.py(联想商城签到)
 Author: marigold233,AlanZhao
 Date: 2023/5/27 11:05
-12 17 54 * * *
+cron: 20 40 12 * *
 new Env('联想商城签到');
 """
 
